93f87d2f4c2705685f5e5a049bc5df4d3722e3f7
RIFQIAFK <rifqiaryandinataa@gmail.com>
2026-04-30 12:25:22 +0700

    Fix: Correct Supabase API Key for database connection

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://pacbybeqsmazefvdzggj.supabase.co';
-const supabaseKey = 'sb_publishable_kQT3uCgJlkt09udppx3CAA_llDU_NmB';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhY2J5YmVxc21hemVmdmR6Z2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODUyNjMsImV4cCI6MjA5MzA2MTI2M30.WHCv0EzkMLKI6-jIatF8bucKTkmmZEA1sPXXO9r4edY';
 
 // Initialize Supabase if the CDN script is loaded
 const supabase = window.supabase ? window.supabase.createClient(supabaseUrl, supabaseKey) : null;
